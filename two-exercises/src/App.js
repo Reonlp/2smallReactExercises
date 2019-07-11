b5c7f8ed731b0e60e70e7fcb704e0797e5d91fc1
Sergio Leon <leonsergio0011@gmail.com>
2019-07-11 18:14:29 +0200

    2 little REACTjs exercises to practice state, props and events

---
+++ two-exercises/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CoinContainer from './CoinContainer';
+import Box from './Box';
+import BoxContainer from "./BoxContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <CoinContainer />
+      <BoxContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ two-exercises/src/Coin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./Coin.css";
+class Coin extends Component {
+    render(){
+        return (
+        <div className='Coin'>
+            <img src={this.props.info.imgSrc}/>
+        </div>
+        )
+    }
+}
+
+export default Coin;
